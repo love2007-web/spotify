4af2f937d880ec8e044c459f35d57fdc34d47002
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-06-21 09:07:27 +0100

    Done with Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <Navbar/>
+    <Sidebar/>
   );
 }
 
